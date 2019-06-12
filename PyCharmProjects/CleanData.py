2fd41bbe8a0a18c0f1df268de0b93b5aaa49802e
kevinminors <44020606+kevinminors@users.noreply.github.com>
2019-06-12 17:15:03 +0100

    Saving dataframe, cleaning more files

--- PyCharmProjects/CleanData.py
+++ PyCharmProjects/CleanData.py
@@ -1,13 +1,11 @@
 import numpy as np
 import pandas as pd
 import datetime as dt
-# import math
 import os
 
-# look for entries with None as timecategory
-# check for journey time = 0
-# todo implement congestion factor method
+# todo check code is saving properly
 # todo implement percentile factor method
+
 # Use import cProfile
 #
 # import cProfile
@@ -44,38 +42,43 @@ def find_multiple_raw_data():
 
     for file in files:
 
+        def converter_function(value):
+            if value == '':
+                return -1
+            return int(value)
+
         if 'fhv' in file:
-            print('Reading data...')
+            print('Reading data "fhv"...')
             file_data = pd.read_csv(file,
                                     parse_dates=[0, 1],
                                     infer_datetime_format=True,
                                     usecols=[0, 1, 2, 3],
                                     names=column_names,
-                                    converters={2: np.int64, 3: np.int64},
+                                    converters={2: converter_function, 3: converter_function},
                                     header=0)
             print('Data read.')
             data = pd.concat([data, clean_raw_data(file_data)], ignore_index=True)
 
         elif 'yellow' in file:
-            print('Reading data...')
+            print('Reading data "yellow"...')
             file_data = pd.read_csv(file,
                                     parse_dates=[0, 1],
                                     infer_datetime_format=True,
                                     usecols=[1, 2, 7, 8],
                                     names=column_names,
-                                    converters={7: np.int64, 8: np.int64},
+                                    converters={7: converter_function, 8: converter_function},
                                     header=0)
             print('Data read.')
             data = pd.concat([data, clean_raw_data(file_data)], ignore_index=True)
 
         elif 'green' in file:
-            print('Reading data...')
+            print('Reading data "green"...')
             file_data = pd.read_csv(file,
                                     parse_dates=[0, 1],
                                     infer_datetime_format=True,
                                     usecols=[1, 2, 5, 6],
                                     names=column_names,
-                                    converters={5: np.int64, 6: np.int64},
+                                    converters={5: converter_function, 6: converter_function},
                                     header=0)
             print('Data read.')
             data = pd.concat([data, clean_raw_data(file_data)], ignore_index=True)
@@ -113,7 +116,9 @@ def calculate_average_journey_time(data):
 def run_congestion_factor_method(c_data, h_data, congestion_factor):
     print('Running congestion method...')
 
-    c_data['IsCongested'] = [c_data.iloc[index, 0] >=
+# todo check values of h_data and c_data. Getting runtime errors
+
+    c_data['IsCongested'] = [c_data.iloc[index, 0] >
                              congestion_factor * h_data.loc[c_data.iloc[index, 1], c_data.iloc[index, 2]]
                              for index in range(c_data.shape[0])]
     c_data['CongestionTime'] = [congestion_factor * h_data.loc[c_data.iloc[index, 1], c_data.iloc[index, 2]]
@@ -132,5 +137,6 @@ historical_data = calculate_average_journey_time(clean_data)
 # print(historical_data)
 congested_data = run_congestion_factor_method(clean_data, historical_data, congestion_factor=100)
 print(congested_data[congested_data['IsCongested']])
+congested_data.to_csv('M:/GitHub/Anomaly-Detection/PyCharmProjects')
 
 
